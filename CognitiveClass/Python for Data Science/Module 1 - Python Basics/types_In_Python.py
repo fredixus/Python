8e8b201d65d8df876877560c9a324bd0a311f6e1
Michał Dębosz <michadbsz@gmail.com>
2018-12-17 18:39:03 +0100

    Create types_In_Python.py

---
+++ CognitiveClass/Python for Data Science/Module 1 - Python Basics/types_In_Python.py
@@ -0,0 +1,21 @@
+#Types
+
+type(11)
+type(11.1)
+type("11")
+
+"""
+QUESTION 1  
+What is the type of the following int(1.0)?
+a)float
+--> b)int 
+c)bool
+"""
+
+"""
+QUESTION 2
+Enter the code to convert the number 1 to a Boolean ?
+____________
+
+--> bool(1)
+"""
