53fb2284cf00c0f22c7e1c042de6cb1419b1e58f
Michał Dębosz <michadbsz@gmail.com>
2018-12-17 18:44:49 +0100

    Create Lab.py

---
+++ CognitiveClass/Python for Data Science/Module 1 - Python Basics/Lab.py
@@ -0,0 +1,19 @@
+"""
+Lab Instructions
+
+How do I run Python?
+You can use the CC Labs environment to run Python code in Jupyter notebooks, by simply clicking on the "Start Lab" button below.
+
+Do I need to download or install Python?
+No, you can use Python directly within your web browser, through CC Labs. Just click on the "Start Lab" button below and CC Labs will load directly on this page.
+
+Which version is used in these labs: Python 2 or Python 3?
+All of these labs use Python 3.
+
+What is "CC Labs"?
+CC Labs is a virtual lab environment that lets you practice what you learn in these courses.
+
+What are "Jupyter Notebooks"?
+Jupyter Notebooks are a popular data science tool that lets Data Scientists write code, see the results, and describe what's happening -- all in a single document. 
+"""
+
