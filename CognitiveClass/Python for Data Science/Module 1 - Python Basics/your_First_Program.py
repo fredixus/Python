867892f28baa767592d22263ab78e944f1077b9e
Michał Dębosz <michadbsz@gmail.com>
2018-12-17 18:35:20 +0100

    Create your_First_Program.py

---
+++ CognitiveClass/Python for Data Science/Module 1 - Python Basics/your_First_Program.py
@@ -0,0 +1,21 @@
+#First program in python -- this is comment
+
+print("Argument of function print")
+
+"""
+QUESTION 1
+Using the Practice Lab find the result of executing print("Hello\nWorld!") statement
+a)Hello\nWorld!
+b)Hello World!'
+--> c)Hello
+
+World!'
+"""
+
+"""
+QUESTION 2
+what is the output of executing the following statement: # print('Hello World!')
+a)Hello World!
+b)"Hello World!"
+--> c)there is no output as it is a comment there is no output as it is a comment - correct
+"""
