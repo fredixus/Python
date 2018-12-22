dbaa00c90d3ee6087b32982bef2b17714fa51b64
Michał Dębosz <michadbsz@gmail.com>
2018-12-22 12:05:53 +0100

    Create 1_listAndTuples.py

---
+++ CognitiveClass/Python for Data Science/Module 2 - Python Data Structures/1_listAndTuples.py
@@ -0,0 +1,17 @@
+#QUESTION 1
+"""
+how do you access the last element of the tuple: A=(0,1,2,3) :
+[*] A[3]
+[] A[0]
+[*] A[-1]
+[] A[end]
+"""
+
+#QUESTION 1
+"""
+Consider the list B=["a","b","c"], what is the result of the following B[1:]
+() []
+() 'b'
+(*) ['b','c']
+() ['a','b', 'c']
+"""
