5f649ca2962f9c34eea9e538d30dbad196ad8935
Michał Dębosz <michadbsz@gmail.com>
2018-12-23 13:08:12 +0100

    Create 5_quizz.py

---
+++ CognitiveClass/Python for Data Science/Module 2 - Python Data Structures/5_quizz.py
@@ -0,0 +1,65 @@
+"""
+Instructions for Review Questions
+
+1. Time allowed: Unlimited  
+  - We encourage you to go back and review the materials to find the right answer
+  - Please remember that the Review Questions are worth 50% of your final mark.
+2. Attempts per question:
+  - One attempt - For True/False questions
+  - Two attempts - For any question other than True/False
+3.  Clicking the "Final Check" button when it appears, means your submission is FINAL.  
+    You will NOT be able to resubmit your answer for that question ever again
+4. Check your grades in the course at any time by clicking on the "Progress" tab
+"""
+
+"""
+QUESTION 1 
+What is the syntax to obtain the first element of the tuple:
+
+A=('a','b','c')
+() A[1]
+(*) A[0]
+() A[:]
+"""
+
+"""
+QUESTION 2 
+After applying the following method,L.append(['a','b']), the following list will only be one element longer.
+
+(*) True
+() False
+"""
+
+"""
+QUESTION 3 
+How many duplicate elements can you have in a set?
+
+() 1
+(*) 0, you can only have one unique element in a set 0, you can only have one unique element in a set - correct
+() 100
+() depends on the number of elements in your set.
+"""
+
+"""
+QUESTION 4 
+Consider the following Python Dictionary:
+Dict={"A":1,"B":"2","C":[3,3,3],"D":(4,4,4),'E':5,'F':6}
+, what is the result of the following operation: Dict["D"]
+
+() 4
+() 3
+() [3,3,3]
+(*) (4,4,4)
+() error
+"""
+
+"""
+QUESTION 5 
+What is an important difference between lists and tuples?
+
+() Lists can't contain a string
+() Tuples can only have integers
+() Lists and tuples are the same.
+(*) Lists are mutable tuples are not
+()  There is no zeros in lists
+"""
