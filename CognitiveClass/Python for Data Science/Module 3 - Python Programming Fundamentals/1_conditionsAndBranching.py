a70c3505066bd9770a02e228a05cea5aa60e6ae3
Michał Dębosz <michadbsz@gmail.com>
2018-12-28 11:48:36 +0100

    Create 1_conditionsAndBranching.py

---
+++ CognitiveClass/Python for Data Science/Module 3 - Python Programming Fundamentals/1_conditionsAndBranching.py
@@ -0,0 +1,27 @@
+"""
+QUESTION 1
+Find the values of i that produces a True for the following:
+i!=0
+
+[*] 1  
+[] 0  
+[*] -1  
+[*] 100000000
+"""
+
+"""
+QUESTION 2
+What is the output of the following:
+x='a'
+if(x!='a'):
+print("this is not a")
+else:
+print("this is a")
+
+() "this is not a"   
+(*) "this is a" 
+"""
+
+#Labs
+#https://gist.github.com/bdc95bdb463146c7ad92cd1fe167b762
+
