cff5dae6e444d8dd025510f477290640022264ba
Michał Dębosz <michadbsz@gmail.com>
2018-12-28 12:10:13 +0100

    Create 2_loops.py

---
+++ CognitiveClass/Python for Data Science/Module 3 - Python Programming Fundamentals/2_loops.py
@@ -0,0 +1,25 @@
+"""
+QUESTION 1
+what is the output of the following lines of code:
+A=[3,4,5]
+for a in A:
+print(a)
+
+() 1,2,3  
+() 0,1,2  
+(*) 3,4,5 
+
+"""
+"""
+QUESTION 2
+what is the output of the following lines of code:
+x=3
+y=1
+while(y!= x):
+  print(y)
+  y=y+1
+  
+  
+(*) 1,2 
+() 0,1 
+() 0
