1aaff36ba3270c929a047f9e3dd578e4910964c5
Michał Dębosz <michadbsz@gmail.com>
2018-12-28 13:54:35 +0100

    Create 3_functions.py

---
+++ CognitiveClass/Python for Data Science/Module 3 - Python Programming Fundamentals/3_functions.py
@@ -0,0 +1,28 @@
+#QUESTION 1
+"""
+What is the value of c after the following block of code is run ?
+a=1
+def add(b):
+  return a+b
+c=add(10)
+
+() 0  
+() 1  
+(*) 11  
+() 111
+"""
+
+#QUESTION 2
+"""
+What is the value of c after the following block of code is run ?
+def f(*x):
+  return sum(c)
+
+() return the total of a variable amount of parameters 
+() return the total of a list  
+(*) the function is not valid
+"""
+
+#Lab
+#https://gist.github.com/e3f01ef8d3b870312b488ad4b4b8a0de
+
