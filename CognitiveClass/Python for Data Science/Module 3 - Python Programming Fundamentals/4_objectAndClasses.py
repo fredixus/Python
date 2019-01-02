ae6480c7c8c4a8c2dff7d15698c29bfe5356575e
Michał Dębosz <michadbsz@gmail.com>
2019-01-02 19:22:10 +0100

    Create 4_objectAndClasses.py

---
+++ CognitiveClass/Python for Data Science/Module 3 - Python Programming Fundamentals/4_objectAndClasses.py
@@ -0,0 +1,31 @@
+#OBJECT AND CLASSES
+
+"""
+QUESTION 1
+
+Using the Class Car in the lab, create a Car object with the following attributes:
+
+make="Honda"
+model="Accord"
+color="blue"
+
+[*] Car(make="Honda",model="Accord",color="blue")  
+[*] Car("Honda","Accord","blue")  
+[*] Car(model="Accord",make="Honda",color="blue")  
+[] Car("Accord","Honda","blue")
+"""
+
+"""
+QUESTION 2
+From the lab how would you change the data attribute "owner_number":
+
+(*) utilising the method sell()  
+() utilising the method car_info()
+"""
+screen =r"Quiz: https://gist.github.com/11b9afae745e8a9a419520647935b128" 
+print(screen)
+
+#Labs
+#https://gist.github.com/d2f0ec86df22fe4dfb9ed5481f6d2c1e
+
+
