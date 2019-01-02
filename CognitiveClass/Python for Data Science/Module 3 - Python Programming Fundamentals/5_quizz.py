4b5614094e6e475ba2607a5063371fe826f032cb
Michał Dębosz <michadbsz@gmail.com>
2019-01-02 19:34:06 +0100

    Create 5_quizz.py

---
+++ CognitiveClass/Python for Data Science/Module 3 - Python Programming Fundamentals/5_quizz.py
@@ -0,0 +1,83 @@
+"""
+Instructions for Review Questions
+
+1. Time allowed: Unlimited  
+ - We encourage you to go back and review the materials to find the right answer
+ - Please remember that the Review Questions are worth 50% of your final mark.
+ 
+2. Attempts per question:
+ - One attempt - For True/False questions
+ - Two attempts - For any question other than True/False
+ 
+3. Clicking the "Final Check" button when it appears, means your submission is FINAL.  
+You will NOT be able to resubmit your answer for that question ever again
+
+4. Check your grades in the course at any time by clicking on the "Progress" tab
+"""
+
+"""
+QUESTION 1 
+What is the output of the following lines of code:
+
+x=1
+if(x!=1):
+  print('Hello')
+else:
+  print('Hi')
+print('Mike')
+
+(*) Hi Mike 
+() Mike  
+() Hello Mike  
+() The Mike
+"""
+
+"""
+QUESTION 2 
+What is the output of the following few lines of code ?:
+
+A=['1','2','3']
+for a in A:
+  print(2*a)
+
+() 2 4 6  
+() '2' '4' '6'  
+(*) '11' '22' '33'  
+() A B C
+"""
+
+"""
+QUESTION 3  (1 point possible)
+Consider the function Delta, when will the function return a value of 1
+
+def Delta(x):
+  if x==0:
+    y=1;
+  else:
+    y=0;
+  return(y)
+  
+() When the input is anything but 0  
+() When the input in 1  
+() Never  
+(*) When the input is 0
+"""
+
+"""
+QUESTION 4 
+What is the correct way to sort the list 'B' using a method, the result should not return a new list, just change the list 'B'.
+
+(*) B.sort()  
+() sort(B)  
+() sorted(B)  
+() B.sorted()
+"""
+
+"""
+QUESTION 5
+what are the keys of the of the following {'a':1,'b':2}
+
+() 1,2  
+() ;,:  
+(*) a,b
+"""
