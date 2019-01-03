623fb5d135d99f43685d274bdd1c38fa8eda0b22
Michał Dębosz <michadbsz@gmail.com>
2019-01-03 17:33:53 +0100

    Update 1_readingFilesWithOpen.py

--- CognitiveClass/Python for Data Science/Module 4 - Working with Data in Python/1_readingFilesWithOpen.py
+++ CognitiveClass/Python for Data Science/Module 4 - Working with Data in Python/1_readingFilesWithOpen.py
@@ -19,3 +19,44 @@ with open("example.txt","r") an File1:
 
 print(File1.closed)
 print(file_stuff)
+
+"""
+print(file_stuff)
+return the file:
+line 1,
+line 2,
+...
+line x.
+x < file.length()
+"""
+#you can take every element in file as a list object
+
+with open("example.txt","r") an File1:
+  file_stuff = File1.readlines()
+  print(file_stuff)
+  
+"""
+return the:
+['line 1','line 2',...,line x]
+x < file.length()
+"""
+
+with open("example.txt","r") an File1:
+  file_stuff = File1.readline()
+  print(file_stuff)
+#print single line of file
+  
+with open("example.txt","r") an File1:
+  for line in File1:
+     print(line)
+#print every single line of file   
+
+with open("example.txt","r") an File1:
+  file_stuff = File1.readlines(Z) #Z - any number - specify the number of character from every line.
+  print(file_stuff)
+ 
+#LABS
+#https://gist.github.com/c1962f8cf7f0dc739d4e60b1ee3b5a2d
+
+
+  
