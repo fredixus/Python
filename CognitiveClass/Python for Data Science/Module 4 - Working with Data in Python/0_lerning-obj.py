0722f73ef08588b42dec383b0f3de98084ce388a
Michał Dębosz <michadbsz@gmail.com>
2019-01-03 17:10:48 +0100

    Create 0_lerning-obj.py

---
+++ CognitiveClass/Python for Data Science/Module 4 - Working with Data in Python/0_lerning-obj.py
@@ -0,0 +1,9 @@
+showMessage = "
+\n In this lesson you will learn about:
+\n \t Reading files with open
+\n \t Writing files with open
+\n \t Loading data with Pandas
+\n \t Working with and Saving data with Pandas
+"
+
+print(showMessage)
