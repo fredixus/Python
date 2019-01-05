a50e8d058263ea178bec29f52b759906fe1c5f92
Michał Dębosz <michadbsz@gmail.com>
2019-01-05 18:46:31 +0100

    Create 6_quizz.py

---
+++ CognitiveClass/Python for Data Science/Module 4 - Working with Data in Python/6_quizz.py
@@ -0,0 +1,50 @@
+"""
+Instructions for Review Questions
+
+1.Time allowed: Unlimited  
+  - We encourage you to go back and review the materials to find the right answer
+  - Please remember that the Review Questions are worth 50% of your final mark.
+2. Attempts per question:
+  - One attempt - For True/False questions
+  - Two attempts - For any question other than True/False
+3. Clicking the "Final Check" button when it appears, means your submission is FINAL.  You will NOT be able to resubmit your answer for that question ever again
+4. Check your grades in the course at any time by clicking on the "Progress" tab
+"""
+
+"""
+QUESTION 2
+What do the following lines of code do?
+with open("Example2.txt","w") as writefile:
+  writefile.write("This is line A\n")
+  writefile.write("This is line B\n")
+  
+() Read the file "Example1.txt"  
+(*) Write to the file “Example1.txt”  
+() Append the file "Example1.txt"
+"""
+
+"""
+QUESTION 3
+What do the following lines of code do?
+with open("Example3.txt","a") as file1:
+  file1.write("This is line C\n")
+  
+() Read the file "Example1.txt"  
+() Write to the file “Example1.txt”  
+(*) Append the file "Example1.txt"t"
+"""
+
+"""
+QUESTION 4
+What is the result of applying the following method df.head(), to the dataframe df
+
+() prints the first row of the dataframe  
+() prints the first column of the dataframe  
+(*) prints the first 5 rows of the dataframe  
+() prints the dateframe out
+"""
+
+
+
+
+
