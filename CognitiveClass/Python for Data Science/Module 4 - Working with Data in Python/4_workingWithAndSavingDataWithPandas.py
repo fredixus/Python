eb5a2da448fb2aeef8730ca63c74464cf2875d59
Michał Dębosz <michadbsz@gmail.com>
2019-01-05 18:31:36 +0100

    Create 4_workingWithAndSavingDataWithPandas.py

---
+++ CognitiveClass/Python for Data Science/Module 4 - Working with Data in Python/4_workingWithAndSavingDataWithPandas.py
@@ -0,0 +1,20 @@
+#Lecture
+
+"""
+QUESTION 1
+consider the dataframe df=pd.DataFrame({'a':[1,2,1],'b':[1,1,1]})
+what type does the following return df['a']==1
+
+() int  
+(*) bool  
+() float
+"""
+
+"""
+QUESTION 2
+what task does the following method perform
+df.to_csv("file.csv")
+
+(*) save a dataframe to a csv file  
+() loading a csv file to a dataframe
+"""
