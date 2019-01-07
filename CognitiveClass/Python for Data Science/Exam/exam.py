e5847cc914d2f9382ac198f4b82da9d33b272c32
Michał Dębosz <michadbsz@gmail.com>
2019-01-07 20:11:20 +0100

    Create exam.py

---
+++ CognitiveClass/Python for Data Science/Exam/exam.py
@@ -0,0 +1,18 @@
+"""
+Exam Instructions
+
+1. This exam is worth 50% of your entire grade for the course
+2. There is no pass/fail for the exam itself, but the grade you get will affect your overall passing grade for the course
+3. Time allowed: 1 hour
+4. Attempts per question:
+    - One attempt - For True/False questions
+    - Two attempts - For any question other than True/False
+    
+5. Clicking the "Final Check" button when it appears, means your submission is FINAL.  
+  You will NOT be able to resubmit your answer for that question ever again
+
+6. Check your grades in the course at any time by clicking on the "Progress" tab
+
+IMPORTANT: Do not let the time run out and expect the system to grade you automatically. 
+You must explicitly submit your answers, otherwise they would be marked as incomplete.
+"""
