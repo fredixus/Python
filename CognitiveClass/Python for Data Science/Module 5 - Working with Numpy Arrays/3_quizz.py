6d5b0eb7c2f512136134f14cd5a8a0feb5bcc756
Michał Dębosz <michadbsz@gmail.com>
2019-01-07 19:47:30 +0100

    Create 3_quizz.py

---
+++ CognitiveClass/Python for Data Science/Module 5 - Working with Numpy Arrays/3_quizz.py
@@ -0,0 +1,45 @@
+"""
+MULTIPLE CHOICE
+what is the result of the following lines of code:
+a=np.array([0,1,0,1,0])
+b=np.array([1,0,1,0,1])
+a*b
+
+() 0 
+() array([1, 1, 1, 1, 1])  
+() array([0, 0, 0, 0, 0])
+
+"""
+
+"""
+MULTIPLE CHOICE
+what is the result of the following lines of code:
+a=np.array([0,1])
+b=np.array([1,0])
+np.dot(a,b)
+
+() 1  
+() array([1,1])  
+() 0  
+() array([0,0])
+"""
+
+"""
+MULTIPLE CHOICE
+what is the result of the following lines of code:
+a=np.array([1,1,1,1,1])
+a+10
+
+() array([10,10,10,10,10])  
+() array([11, 11, 11, 11, 11])  
+() array([1,1,1,1,1])
+"""
+
+"""
+MULTIPLE CHOICE
+what is the correct code to perform matrix multiplication on the matrix A and B
+
+() np.dot(A,B)
+() A*B 
+() AxB
+"""
