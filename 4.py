667ee8f932362043b9ba62a1cdc3c5248c34a951
Michal Debosz <michadbsz@gmail.com>
2019-01-15 19:50:42 +0100

    add if mislead to show diferance

--- 4.py
+++ 4.py
@@ -8,9 +8,16 @@ years = [2013,2014]
 plt.bar(years, mention, 0.8)
 plt.xticks(years)
 
-plt.ylabel("Ile razy usłyszałem frazę nauka o danych?")
+plt.ylabel("Ile razy uslyszalem fraze nauka o danych?")
 
 plt.ticklabel_format(useOffset=False)
 
-#plt.axis([2012.5,2014.5,499,506])
+mislead = True
+
+if mislead:
+    plt.axis([2012.5,2014.5,499,506])
+    plt.title("Look at the 'Huge' Increase!")
+else:
+    plt.axis([2012.5,2014.5,0,550])
+    plt.title("Not So Huge Anymore.")
 plt.show()
