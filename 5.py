30c99202c526d191c7aa3c3ace498e68806d6775
Michal Debosz <michadbsz@gmail.com>
2019-01-17 18:11:03 +0100

    added lines

--- 5.py
+++ 5.py
@@ -6,3 +6,14 @@ variance        = [1,2,4,8,16,32,64,128,256]
 bias_squared    = [256,128,64,32,16,8,4,2,1]
 
 total_error = [x + y for x,y in zip(variance,bias_squared)]
+
+xs = [i for i,_ enumerate(variance)]
+
+plt.plot(xs,variance,'g-',label='variance')
+plt.plot(xs,bias_squared ,'r-',label='bias^2')
+plt.plot(xs,total_error ,'b:',label='total error')
+
+plt.legend(loc=9)
+plt.xlabel("Stopień skomplikowania modelu")
+plt.title("Kompromis między wartością progową i zlozonoscia modelu")
+plt.show()
