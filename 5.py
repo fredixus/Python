f8d61143af5f819f94a186765e237a404447841b
Michal Debosz <michadbsz@gmail.com>
2019-01-17 18:14:06 +0100

    make file simpler to read (add tabs)

--- 5.py
+++ 5.py
@@ -5,15 +5,15 @@ from matplotlib import pyplot as plt
 variance        = [1,2,4,8,16,32,64,128,256]
 bias_squared    = [256,128,64,32,16,8,4,2,1]
 
-total_error = [x + y for x,y in zip(variance,bias_squared)]
+total_error     = [x + y for x,y in zip(variance,bias_squared)]
 
-xs = [i for i,_ enumerate(variance)]
+xs              = range(len(variance))
 
-plt.plot(xs,variance,'g-',label='variance')
-plt.plot(xs,bias_squared ,'r-',label='bias^2')
-plt.plot(xs,total_error ,'b:',label='total error')
+plt.plot    (xs,variance,       'g-',   label='variance')
+plt.plot    (xs,bias_squared ,  'r-.',  label='bias^2')
+plt.plot    (xs,total_error ,   'b:',   label='total error')
 
-plt.legend(loc=9)
-plt.xlabel("Stopień skomplikowania modelu")
-plt.title("Kompromis między wartością progową i zlozonoscia modelu")
-plt.show()
+plt.legend  (loc=9)
+plt.xlabel  ("Stopien skomplikowania modelu")
+plt.title   ("Kompromis miedzy wartoscia progowa i zlozonoscia modelu")
+plt.show    ()
