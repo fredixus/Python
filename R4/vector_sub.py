bd1c4d398d6fd8304a5c19bc1f8d8f482f42486f
Michal Debosz <michadbsz@gmail.com>
2019-03-16 14:40:26 +0100

    change name from 4.3 to vector_sub

--- R4/vector_sub.py
+++ R4/vector_sub.py
@@ -3,7 +3,7 @@
 Subtraction (-) two vectors 
 example: [a = (1,2); b = (3,2): a - b: (-2,0)]
 """
-def vector_add(v, w):
+def vector_sub(v, w):
     """(-)"""
     return [v_i - w_i for v_i, w_i in zip(v,w)]
 
