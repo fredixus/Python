98d5efdc735163f388c7c39a8152293f16bbd476
Michal Debosz <michadbsz@gmail.com>
2019-03-16 14:14:44 +0100

    add new function substraction of two vectors

--- R4/4_3.py
+++ R4/4_3.py
@@ -1,4 +1,12 @@
 """
-Add (+) two vectors 
-example: [a = (1,2); b = (3,2): a + b: (4,4)]
+Subtraction (-) two vectors 
+example: [a = (1,2); b = (3,2): a - b: (-2,0)]
 """
+def vector_add(v, w):
+    """(-)"""
+    return [v_i - w_i for v_i, w_i in zip(v,w)]
+
+a = [1,2] 
+b = [3,2]
+
+print(vector_add(a,b))
