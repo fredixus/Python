8b7bc26669541c7f0bc9fafa614be52cb8237a6d
Michal Debosz <michadbsz@gmail.com>
2019-03-18 18:30:59 +0100

    add a example, convert result to float/double

--- R4/vector_mean.py
+++ R4/vector_mean.py
@@ -7,4 +7,11 @@ from scalar_multiply import scalar_multiply
 
 def vector_mean(vectors):
     n = len(vectors)
-    return scalar_multiply(1/n,vector_sum(vectors))
+    return scalar_multiply(1.0/n,vector_sum(vectors))
+
+# a = [1,1] 
+# b = [8,8]
+# c = [3,3]
+
+# sum = [a]+[b]+[c]
+# print(vector_mean(sum))
