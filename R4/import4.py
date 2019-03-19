69c803615825ce415ae8e29f37ee6fc0ca05869f
Michal Debosz <michadbsz@gmail.com>
2019-03-19 18:11:56 +0100

    new file import4 with f(imports) include linear alg

---
+++ R4/import4.py
@@ -0,0 +1,23 @@
+"""
+Import all of Linear Algebra
+"""
+
+def imports():
+    """ Import all of Linear Algebra: 
+    matrix as mx, 
+    distance as distance, 
+    magniture as magnitude, 
+    scalar_multiply as scalar_multiply, 
+    squered_distance as squered_distance,  
+    sum_of_squers, vector_add, vector_dot, vector_mean, vector_sub, vector_sum"""
+    import matrix as mx
+    import distance as distance
+    import magniture as magnitude
+    import scalar_multiply as scalar_multiply
+    import squered_distance as squered_distance
+    import sum_of_squers as sum_of_squers
+    import vector_add as vector_add
+    import vector_dot as vector_dot
+    import vector_mean as vector_mean
+    import vector_sub as vector_sub
+    import vector_sum as vector_sum
