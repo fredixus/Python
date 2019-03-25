6b1113f4e292292515820d01a618b923473b4310
Michal Debosz <michadbsz@gmail.com>
2019-03-25 17:26:27 +0100

    add median to calculate mid value of vector

--- R5/stat1.py
+++ R5/stat1.py
@@ -5,12 +5,18 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import math
 
-num_friends = [100,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+num_friends         = [100,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,
+                        12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,
+                        9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,
+                        7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,
+                        5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
+                        3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,
+                        2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
 def make_friend_counts_histogram(num_friends):
-    friend_counts = Counter(num_friends)
-    xs = range(101)
-    ys = [friend_counts[x] for x in xs]
+    friend_counts   = Counter(num_friends)
+    xs              = range(101)
+    ys              = [friend_counts[x] for x in xs]
     plt.bar(xs, ys)
     plt.axis([0,101,0,25])
     plt.title("Histogram of Friend Counts")
@@ -21,6 +27,24 @@ def make_friend_counts_histogram(num_friends):
 make_friend_counts_histogram(num_friends)
 
 def mean(x):
-    return sum(x)*1.0/len(x)
+    return      sum(x)*1.0/len(x)
 
 print(mean(num_friends))
+
+def median(v):
+    """
+        Mid value of vector
+    """
+
+    n           = len(v)
+    sorted_v    = sorted(v)
+    mid_p       = n // 2
+
+    if(n % 2 == 1):
+      return    sorted_v[mid_p] 
+    else:
+        low     = mid_p-1
+        high    = mid_p
+        return  (sorted_v(low) + sorted_v(high))  
+
+  
