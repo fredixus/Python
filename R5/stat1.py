29416e47d2e2f56053d3eb8073ec2e16f0ea31ba
Michal Debosz <michadbsz@gmail.com>
2019-03-25 18:44:35 +0100

    add correlation

--- R5/stat1.py
+++ R5/stat1.py
@@ -138,3 +138,10 @@ def covariance(x, y):
 
 print(covariance(num_friends, daily_minutes))
 
+def correlation(x, y):
+    stdev_x = standard_deviation(x)
+    stdev_y = standard_deviation(y)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(x, y) / stdev_x / stdev_y
+    else:
+        return 0 # if no variation, correlation is zero
