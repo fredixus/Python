eeb20fb07df2e684d3f57cb7c629f12a8dcf0e40
Michal Debosz <michadbsz@gmail.com>
2019-03-25 18:01:26 +0100

    add quantile

--- R5/stat1.py
+++ R5/stat1.py
@@ -13,7 +13,7 @@ num_friends         = [100,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13
                         3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,
                         2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
-def make_friend_counts_histogram(num_friends):
+def make_friend_counts_histogram (num_friends):
     friend_counts   = Counter(num_friends)
     xs              = range(101)
     ys              = [friend_counts[x] for x in xs]
@@ -24,14 +24,14 @@ def make_friend_counts_histogram(num_friends):
     plt.ylabel  ("# of people")
     plt.show    ()
 
-make_friend_counts_histogram(num_friends)
+#make_friend_counts_histogram(num_friends)
 
-def mean(x):
+def mean (x):
     return      sum(x)*1.0/len(x)
 
 print(mean(num_friends))
 
-def median(v):
+def median (v):
     """ Mid value of vector """
     n           = len(v)
     sorted_v    = sorted(v)
@@ -45,4 +45,13 @@ def median(v):
         return  (sorted_v[low] + sorted_v[high])*1.0 / 2  
 
 print(median(num_friends))
- 
+
+def quantile (x, p):
+    """ Zwraca p-ta wartosc zbioru x """
+    p_index = int(p * len(x))
+    return sorted (x)[p_index]
+
+print("Decyl \t0.10: %s"    % quantile(num_friends,0.10))
+print("Kwantyl 0.25: %s"    % quantile(num_friends,0.25))
+print("Kwantyl 0.75: %s"    % quantile(num_friends,0.75))
+print("Decyl \t0.10: %s"    % quantile(num_friends,0.90))
