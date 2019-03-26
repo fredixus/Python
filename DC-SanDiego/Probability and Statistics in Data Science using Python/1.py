f8198e5adbb66415f6d0a810e50c67c5719647ca
Michal Debosz <michadbsz@gmail.com>
2019-03-26 20:15:14 +0100

    add links to lectures

--- DC-SanDiego/Probability and Statistics in Data Science using Python/1.py
+++ DC-SanDiego/Probability and Statistics in Data Science using Python/1.py
@@ -5,3 +5,9 @@ def gen_c(k=1000, n=100):
     return S
 
 print(gen_c)
+#1-https://youtu.be/DMz_0nuk9aw
+#2-https://youtu.be/b72IgkoI8cQ
+#3-https://youtu.be/d-wI6Y1nQ_s
+#4-https://youtu.be/ISfcSsEMhek
+#5a-https://youtu.be/KxYzeEUvOQo
+#5b-https://youtu.be/vwJj6lScr6o
