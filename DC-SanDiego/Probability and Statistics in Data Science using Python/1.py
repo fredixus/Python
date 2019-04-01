f0a84b669ca40c9d18be06d5cdb86b2190a5bd0a
Michal Debosz <michadbsz@gmail.com>
2019-04-01 18:03:35 +0200

    new file added

--- DC-SanDiego/Probability and Statistics in Data Science using Python/1.py
+++ DC-SanDiego/Probability and Statistics in Data Science using Python/1.py
@@ -1,13 +1,26 @@
 
 def gen_c(k=1000, n=100):
-    X = 2 * (random.rand(k,n)>0.5)-1 
-    S = sum (X,axis=0)
+    #X = 2 * (random.rand(k,n)>0.5)-1 
+    S = sum (k,axis=0)
     return S
 
 print(gen_c)
+#Stage 1
+
 #1-https://youtu.be/DMz_0nuk9aw
 #2-https://youtu.be/b72IgkoI8cQ
 #3-https://youtu.be/d-wI6Y1nQ_s
 #4-https://youtu.be/ISfcSsEMhek
 #5a-https://youtu.be/KxYzeEUvOQo
 #5b-https://youtu.be/vwJj6lScr6o
+
+#Stage 2
+
+#https://youtu.be/l4yGL-B0hb8
+#https://youtu.be/SARNis9dDtk
+#https://youtu.be/EuEZ6U4pE8o
+#Set Relations - on hard drive
+#https://youtu.be/_DsyekLOXBw
+#https://youtu.be/UfKk6n3QS_0
+#Russell's Paradox - on hard drive
+#
